2bfb709dfd5c1b5f7e633ffbd1926d968c3af49e
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-08-18 11:52:38 -0500

    archivos iniciales

---
+++ index.php
@@ -0,0 +1,22 @@
+<<!--
+http://www.youtube.com/watch?v=hoAEhGh0xwY
+
+Archivos e: www.mediafire.com/download/2734w2k94103nd9/ArchivosIniciales.rar
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Carrito de Compras</title>
+	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
+	<script type="text/javascript" href="./js/scripts.js"></script>
+</head>
+<body>
+	<header>
+		<h1>Carrito de Compras</h1>
+		<a href="./carritodecompras.php" title="Ver carrito de compras"></a>
+		<img src="./imagenes/carrito.png">
+	</header>
+</body>
+</html>
