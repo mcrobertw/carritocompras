5ce82f88ccace538eaa4ec491602443bacf31e9e
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-08-26 14:54:15 -0500

    terminando hasta el video 4

--- carritodecompras.php
+++ carritodecompras.php
@@ -1,6 +1,8 @@
 <!--
 	video2:	http://www.youtube.com/watch?v=-Wsw4n2qYRQ
 	video3: http://www.youtube.com/watch?v=G1T2QEM3Tr0
+	video4: https://www.youtube.com/watch?v=BK3EKM0Ir2c
+	video5: https://www.youtube.com/watch?v=1wy5F_q0coQ
 -->
 <?php
 	/*session_unset();
@@ -9,21 +11,46 @@
 	include './conexion.php';
 	if(isset($_SESSION['carrito']))
 	{
-		$arreglo=$_SESSION['carrito'];
-		$encontro=false;
-		$numero=0;
-		for($i=0;i<count($arreglo);$i++){
-			if($arreglo[$i]['Id']==$_GET['id']){
-				$encontro=true;
-				$numero=$i;
-			}
-		}
+		if(isset($_GET['id']))
+		{
+				$arreglo=$_SESSION['carrito'];
+				$encontro=false;
+				$numero=0;
+				for($i=0;$i<count($arreglo);$i++){
+					if($arreglo[$i]['Id']==$_GET['id']){
+						$encontro=true;
+						$numero=$i;
+					}
+				}
 
-		if($encontro==true){
-			$arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']+1;
-			$_SESSION['carrito']=$arreglo;
-		}
+				if($encontro==true){
+					$arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']+1;
+					$_SESSION['carrito']=$arreglo;
+				}else{
 
+					$nombre="";
+					$precio=0;
+					$imagen="";
+
+					$re=mysql_query("select * from producto where id=".$_GET['id']);
+
+					
+					while ($f=mysql_fetch_array($re)) {
+						//print_r($f);
+						$nombre=$f['nombre'];
+						$precio=$f['precio'];
+						$imagen=$f['imagen'];
+					
+					}
+					$datosNuevos=array('Id'=>$_GET['id'],
+										'Nombre'=>$nombre,
+										'Precio'=>$precio,							
+										'Imagen'=>$imagen,
+										'Cantidad'=>1);
+					array_push($arreglo, $datosNuevos);
+					$_SESSION['carrito']=$arreglo;
+				}
+		}
 	}else{
 		if(isset($_GET['id'])){
 			
@@ -57,7 +84,8 @@
 	<meta charset="utf-8"/>
 	<title>Carrito de Compras</title>
 	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
-	<script type="text/javascript" href="./js/scripts.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="./js/scripts.js"></script>
 </head>
 <body>
 	<header>
@@ -81,19 +109,24 @@
 						<img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
 						<span><?php echo $datos[$i]['Nombre'];?></span><br>
 						<span>Precio: <?php echo $datos[$i]['Precio'];?></span><br>
-						<span>Cantidad: <input type="text" value="<?php echo $datos[$i]['Cantidad'];?>"> </span>
+						<span>Cantidad: 
+							<input type="text" value="<?php echo $datos[$i]['Cantidad'];?>"
+							data-precio="<?php echo $datos[$i]['Precio'];?>"
+							data-id="<?php echo $datos[$i]['Id'];?>"
+							class="cantidad"> 
+						</span><br>
 						<span>Subtotal: <?php echo $datos[$i]['Precio']*$datos[$i]['Cantidad'];?></span><br>
 						
 					</center>	
 				</div>
 		<?php	
-				$total=($datos[$i]['Precio']*$datos[$i]['Cantidad']);
+				$total+=($datos[$i]['Precio']*$datos[$i]['Cantidad']);
 
 				}
 			}else{
 				echo '<center><h2>El carro de compras esta vacio</h2></center>';
 			}
-			echo '<center><h2>Total: '. $total .' </h2></center>';
+			echo '<center><h2 id="total">Total: '. $total .' </h2></center>';
 		?>
 		<center><a href="./">Ver catalogo</a></center>
 
