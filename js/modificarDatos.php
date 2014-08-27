76fb0192a8a327b4c809cefb7cfe4136f02c80a0
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-08-27 17:04:00 -0500

    corregido.. no era $post sino $_Post

--- js/modificarDatos.php
+++ js/modificarDatos.php
@@ -1,14 +1,14 @@
 <?php
 	session_start();
 	$arreglo=$_SESSION['carrito'];
-	$total;
+	$total=0;
 	$numero=0;
 	for($i=0;$i<count($arreglo);$i++){
 		if($arreglo[$i]['Id']==$_POST['Id']){
 			$numero=$i;
 		}
 	}
-	$arreglo[$numero]['Cantidad']=$POST['Cantidad'];
+	$arreglo[$numero]['Cantidad']=$_POST['Cantidad'];
 	for($i=0;$i<count($arreglo);$i++){
 		//$total=$total+($arreglo[$i]['Precio']*$arreglo[$i]['Cantidad']);
 		$total=($arreglo[$i]['Precio']*$arreglo[$i]['Cantidad'])+$total;
