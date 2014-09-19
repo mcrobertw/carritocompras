bd51eec6b959bbeb1e889368ad291f98e9d55587
Robert Moreira <robertmoreiracenteno@hotmail.com>
2014-09-19 17:06:26 -0500

    video 10
    concluido envio de mail... no envia correos.. falta configurar mercury
    de xampp

--- compras/compras.php
+++ compras/compras.php
@@ -1,7 +1,8 @@
 <?php
-	include('../conexion.php');
 	session_start();
+	include('../conexion.php');
 	$arreglo=$_SESSION['carrito'];
+	/*
 	$numeroventa=0;
 	$re=mysql_query("select * from compras order by numeroventa DESC limit 1") or die (mysql_error());
 	while($f=mysql_fetch_array($re)){
@@ -23,9 +24,64 @@
 				'".$arreglo[$i]['Cantidad']."',
 				'".($arreglo[$i]['Precio']*$arreglo[$i]['Cantidad'])."'
 			)")or die(mysql_error());
-	}
+	}*/
 
-	unset($_SESSION['carrito']);
-	header("Location: ../admin.php");
+	/*unset($_SESSION['carrito']);
+	header("Location: ../admin.php");*/
+	$total=0;
+	$tabla='<table border="1">
+			<tr>
+				<th>Nombre</th>
+				<th>Cantidad</th>
+				<th>Precio</th>
+				<th>Subtotal</th>
+			</tr>';
+	for($i=0;$i<count($arreglo);$i++){
+		$tabla=$tabla.'<tr>
+			<td>'.$arreglo[$i]['Nombre'].'</td>
+			<td>'.$arreglo[$i]['Cantidad'].'</td>
+			<td>'.$arreglo[$i]['Precio'].'</td>
+			<td>'.$arreglo[$i]['Cantidad']*$arreglo[$i]['Precio'].'</td>
+			</tr>
+		';
+		$total=$total+($arreglo[$i]['Cantidad']*$arreglo[$i]['Precio']);
+	}
+	$tabla=$tabla.'</table>';
+	//echo $tabla;
+	$nombre="Luis Alberto Grijalva";
+	$fecha=date("d-m-Y");
+	$hora=date("H:i:s");
+	$asunto="Compra en X dominio";
+	$desde="www.tupagina.com";
+	$correo="robertmoreiracenteno@gmail.com";
+	$comentario='
+		<div style="
+			border:1px solid #d6d2d2;
+			border-radius:5px;
+			padding:10px;
+			width:800px;
+			height:300px;
+		">
+		<center>
+			<img src="https://yt4.ggpht.com/-3eVnkBJn2y4/AAAAAAAAAAI/AAAAAAAAAAA/hAqolVRolHc/s48-c-k-no/photo.jpg" width="500px" height="250px">
+			<h1>Muchas gracias por comprar en mi carrito de compras</h1>
+		</center>
+			<p>Hola '.$nombre.' muchas gracias por comprar aqui, te mando los detalles</p>
+			<p>Lista de Art&iacute;culos<br>
+				'.$tabla.'
+				<br>
+				Total del pago es: '.$total.'
+			</p>
+		</div>
+	';
+
+	//echo $comentario;
+	$headers="MIME-Version: 1.0\r\n";
+	$headers.="Content-type: text/html; charset=utf8\r\n";
+	$headers.="From: Remitente\r\n";
+	mail($correo,$asunto,$comentario,$headers);
+	//unset($_SESSION['carrito']);
+	//header("Location: ../admin.php");
+
 
 ?>
